72cde9010e6c3b6d4fe47e7f913f0e898ca26e5d
qwq233 <qwq233@qwq2333.top>
2022-03-05 02:55:39 +0800

    gradle: set up dependencies manage plugins
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,30 @@
+/*
+ * EPUB Generator
+ * Copyright (C) 2022 qwq233 qwq233@qwq2333.top
+ * https://github.com/qwq233/epub-generator
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by qwq233.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/qwq233/qwq233/blob/master/eula.md>.
+ */
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20-M1"
+    kotlin("plugin.serialization") version "1.6.20-M1"
     application
     java
 }
@@ -13,8 +36,17 @@ repositories {
     mavenCentral()
 }
 
+val kotlinVersion = "1.6.20-M1"
+
 dependencies {
-    implementation("me.tongfei:progressbar:0.9.3")
+    implementation("me.tongfei:progressbar:_")
+    implementation("com.charleskorn.kaml:kaml:_")
+    implementation("org.dom4j:dom4j:_")
+    implementation("es.nitaur.markdown:txtmark:_")
+    implementation(Kotlin.stdlib.common)
+    implementation(Kotlin.stdlib)
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
+
 }
 
 tasks.withType<KotlinCompile> {
@@ -25,8 +57,17 @@ application {
     mainClass.set("top.qwq2333.MainKt")
 }
 
-tasks.withType<Jar>() {
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
     manifest {
         attributes["Main-Class"] = "top.qwq2333.MainKt"
     }
+    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,34 @@
+/*
+ * EPUB Generator
+ * Copyright (C) 2022 qwq233 qwq233@qwq2333.top
+ * https://github.com/qwq233/epub-generator
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by qwq233.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/qwq233/qwq233/blob/master/eula.md>.
+ */
+
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.40.1"
+////                            # available:"0.30.0"
+////                            # available:"0.30.1"
+////                            # available:"0.30.2"
+////                            # available:"0.40.0"
+////                            # available:"0.40.1"
+}
 
 rootProject.name = "epub-generator"
 
